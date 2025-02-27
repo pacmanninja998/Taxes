cf94855c517f372e693b198759d2d117dbf40daa
pacmanninja998 <pacmanninja998@gmail.com>
2025-02-26 21:55:43 -0500

    Delete hook-Taxes.py
    using pyinstaller Taxes.spec instead

--- hook-Taxes.py
+++
@@ -1,7 +0,0 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
-
-# Collect all submodules
-hiddenimports = ['routes', 'template_manager', 'data_manager']
-
-# Collect data files
-datas = collect_data_files('templates') + collect_data_files('static')
